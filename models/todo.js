12cef9c1b68ad2d36998596c060d4b31f7e6d5c9
lintang <lintangmochz@gmail.com>
2022-11-25 15:45:03 +0700

    modified models todo change table task -> content

--- models/todo.js
+++ models/todo.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
 
-const todoSchema = new Schema({
-    todo: {
+const todoSchema = new mongoose.Schema({
+    content: {
         type: String,
         required: true
     }
 })
 
-const todo = mongoose.model("todo", todoSchema)
+const Todo = mongoose.model('Todo', todoSchema)
 
-module.exports = todo
+module.exports = Todo
