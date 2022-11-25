042991b197fdc48dc817f2cac214bbde7f791786
lintang <lintangmochz@gmail.com>
2022-11-25 15:48:56 +0700

    create CRUD method, initialize .env

--- server.js
+++ server.js
@@ -1,44 +1,74 @@
+require("dotenv").config();
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const port = process.env.PORT
 const mongoose = require('mongoose')
-const Todo = require('./models/todo')
 
-const port = 3000
+// models
+const Todo = require('./models/Todo.js')
 
+// view engine configuration
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
 
-const dburl = 'mongodb://localhost:27017/todolistdb'
-mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
+// connection to db
+mongoose.connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log("Connected to db!");
+
+        app.listen(port, () => console.log('Server is runnning on port: ' + port));
+    })
+    .catch((err) => console.log("Error connecting to the database", err))
+
+// get method
 app.get('/', (req, res) => {
-    Todo.find()
-    .then(result => {
-        res.render('index', { data: result })
-        console.log(result)
+    Todo.find({}, (err, tasks) => {
+        res.render('todo', { todo: tasks })
     })
-})
+});
 
-app.post('/', (req, res) => {
-    const todo = new Todo({
-        todo: req.body.todoValue
+// post method
+app.post('/', async (req, res) => {
+    const addTask = new Todo({
+        content: req.body.content
     })
-    todo.save()
-    .then(result => {
+    try {
+        await addTask.save()
+
         res.redirect('/')
-    })
+    } catch (err) {
+        res.send(err)
+    }
 })
 
-app.delete('/:id', (req, res) => {
-    Todo.findByIdAndDelete(req.params.id)
-    .then(result => {
-        console.log(result)
-    })
-})
+// update method
+app.route('/edit/:id')
+    .get((req, res) => {
+        const id = req.params.id
 
-app.listen(port, () => {
-    console.log('Server is runnning on port: ' + port)
-})
+        Todo.find({}, (err, tasks) => {
+            res.render('todoEdit', { todo: tasks, idTask: id })
+        })
+    })
+    .post((req, res) => {
+        const id = req.params.id
+
+        Todo.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err)
+            res.redirect('/')
+        })
+    })
+
+//DELETE
+app.route("/remove/:id")
+    .get((req, res) => {
+        const id = req.params.id
+
+        Todo.findByIdAndRemove(id, err => {
+            if (err) return res.send(500, err)
+            res.redirect("/")
+        })
+    })
